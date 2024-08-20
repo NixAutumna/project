375dbc58cbac853f990316ff54b996f9244e0a46
NixAutumna <autumnanix@outlook.com>
2024-08-20 15:54:56 +0800

    v2.3

--- py/test.py
+++ py/test.py
@@ -13,21 +13,23 @@ import torch.nn.functional as F
 # while True:
 #     print(Decimal(input()) * Decimal("2") - Decimal("0.01"))
 
-nerror = 0
-berror =0
-P = 0
-I = 0
-D = 0
-PID_value = 0
-self.point.point.x = 0
-ke = 0.01
-ki = 0.01
-kd = 0.01
-self.twist.linear.x = 0.65
-while True:
-    nerror = self.point.point.x-480
-    I = I + nerror
-    D = nerror - berror
-    PID_value = ke * nerror + ki * I + kd * D
-    berror = nerror
-    self.twist.linear.x += PID_value
+# nerror = 0
+# berror =0
+# P = 0
+# I = 0
+# D = 0
+# PID_value = 0
+# self.point.point.x = 0
+# ke = 0.01
+# ki = 0.01
+# kd = 0.01
+# self.twist.linear.x = 0.65
+# while True:
+#     nerror = self.point.point.x-480
+#     I = I + nerror
+#     D = nerror - berror
+#     PID_value = ke * nerror + ki * I + kd * D
+#     berror = nerror
+#     self.twist.linear.x += PID_value
+# for i in range():
+#     print(i)
